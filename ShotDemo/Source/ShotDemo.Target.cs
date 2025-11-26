40762cd31c4db8c5649b201c8145f59775c0a724
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-11-26 19:34:53 +0800

    feat: add class

---
+++ ShotDemo/Source/ShotDemo.Target.cs
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ShotDemoTarget : TargetRules
+{
+	public ShotDemoTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+
+		ExtraModuleNames.AddRange( new string[] { "ShotDemo" } );
+	}
+}
