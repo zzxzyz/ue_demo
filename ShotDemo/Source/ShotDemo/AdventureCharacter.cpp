5d8e99ac1c3b57ddb34af9273ebd77c15643e319
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-11-27 09:57:26 +0800

    feat: add move and jump

--- ShotDemo/Source/ShotDemo/AdventureCharacter.cpp
+++ ShotDemo/Source/ShotDemo/AdventureCharacter.cpp
@@ -15,8 +15,21 @@ AAdventureCharacter::AAdventureCharacter()
 void AAdventureCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("character begin play"));
-	
+
+	check(GEngine != nullptr);
+
+	// Get the player controller for this character
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		// Get the enhanced input local player subsystem and add a new input mapping context to it
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(FirstPersonContext, 0);
+		}
+	}
+	// Display a debug message for five seconds. 
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using AdventureCharacter."));
 }
 
 // Called every frame
@@ -29,7 +42,33 @@ void AAdventureCharacter::Tick(float DeltaTime)
 // Called to bind functionality to input
 void AAdventureCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Check the UInputComponent passed to this function and cast it to an UEnhancedInputComponent
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Bind Movement Actions
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAdventureCharacter::Move);
 
+		// Bind Jump Actions
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+	}
+}
+
+void AAdventureCharacter::Move(const FInputActionValue& Value)
+{
+	// 2D Vector of movement values returned from the input action
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+
+	// Check if the controller posessing this Actor is valid
+	if (Controller)
+	{
+		// Add left and right movement
+		const FVector Right = GetActorRightVector();
+		AddMovementInput(Right, MovementValue.X);
+
+		// Add forward and back movement
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, MovementValue.Y);
+	}
 }
 
--- ShotDemo/Source/ShotDemo/AdventureCharacter.h
+++ ShotDemo/Source/ShotDemo/AdventureCharacter.h
@@ -1,10 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h" 
+#include "InputActionValue.h"
 #include "AdventureCharacter.generated.h"
+
+
+class UInputMappingContext;
+class UInputAction;
+class UInputComponent;
 
 UCLASS()
 class SHOTDEMO_API AAdventureCharacter : public ACharacter
@@ -19,11 +26,26 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputMappingContext> FirstPersonContext;
+
+	// Move Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> MoveAction;
+
+	// Jump Input Actions
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> JumpAction;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Handles 2D Movement Input
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
 
 };
