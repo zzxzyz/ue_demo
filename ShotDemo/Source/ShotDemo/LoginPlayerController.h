a551b183cfad47e86e26cd6e3fc6ce065e0e72bd
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-11-27 16:10:51 +0800

    feat: add login dialog

--- ShotDemo/Source/ShotDemo/LoginPlayerController.h
+++ ShotDemo/Source/ShotDemo/LoginPlayerController.h
@@ -15,27 +15,5 @@ UCLASS()
 class SHOTDEMO_API ALoginPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-
-public:
-	ALoginPlayerController();
-
-	virtual void BeginPlay() override;
-
-protected:
-	// 登录Widget类引用
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
-	TSubclassOf<UUserWidget> LoginWidgetClass;
-
-	// 登录Widget实例
-	UPROPERTY()
-	ULoginWidget* LoginWidgetInstance;
-
-	// 显示登录界面
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void ShowLoginWidget();
-
-	// 隐藏登录界面
-	UFUNCTION(BlueprintCallable, Category = "UI")
-	void HideLoginWidget();
 };
 
