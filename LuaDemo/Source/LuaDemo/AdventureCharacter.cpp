c696ff69caaad0a06c8112cdb5819e542fb6c022
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-12-01 19:46:05 +0800

    feat: add third party

--- LuaDemo/Source/LuaDemo/AdventureCharacter.cpp
+++ LuaDemo/Source/LuaDemo/AdventureCharacter.cpp
@@ -3,6 +3,25 @@
 
 #include "AdventureCharacter.h"
 
+
+#include "base/log.h"
+#include "monitor_usage.h"
+
+void TestMonitorUsage() {
+	static MonitorUsage::Ptr usage = CreateMonitorUsage();
+	static bool b_start = false;
+	if (usage != nullptr) {
+		if (!b_start) {
+			b_start = true;
+			usage->Start();
+		}
+		else {
+			b_start = false;
+			usage->Stop();
+		}
+	}
+}
+
 // Sets default values
 AAdventureCharacter::AAdventureCharacter()
 {
@@ -14,9 +33,31 @@ AAdventureCharacter::AAdventureCharacter()
 // Called when the game starts or when spawned
 void AAdventureCharacter::BeginPlay()
 {
+	TestMonitorUsage();
 	Super::BeginPlay();
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("in advance character"));
 	
 }
+
+// Called when the game ends or when destroyed
+void AAdventureCharacter::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	TestMonitorUsage();
+
+	// Log the EndPlay event with reason
+	const FString ReasonString = UEnum::GetValueAsString(EndPlayReason);
+	LOG_INFO("AdventureCharacter EndPlay - Reason: %s", TCHAR_TO_UTF8(*ReasonString));
+
+	// Display on-screen debug message
+	if (GEngine)
+	{
+		const FString Message = FString::Printf(TEXT("AdventureCharacter EndPlay - Reason: %s"), *ReasonString);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, Message);
+	}
+}
+
 
 // Called every frame
 void AAdventureCharacter::Tick(float DeltaTime)
--- LuaDemo/Source/LuaDemo/AdventureCharacter.h
+++ LuaDemo/Source/LuaDemo/AdventureCharacter.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Called when the game ends or when destroyed
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
