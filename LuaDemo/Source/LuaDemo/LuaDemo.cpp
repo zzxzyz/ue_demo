de94d007995537161adc88499c7ea3a8485a10e5
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-12-01 17:45:32 +0800

    feat: add game mode

---
+++ LuaDemo/Source/LuaDemo/LuaDemo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LuaDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LuaDemo, "LuaDemo" );
