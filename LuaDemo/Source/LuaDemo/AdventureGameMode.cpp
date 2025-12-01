3eac3f8655000c436d58c1457bcae3fab0428efb
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-12-01 19:13:26 +0800

    feat: add ui

--- LuaDemo/Source/LuaDemo/AdventureGameMode.cpp
+++ LuaDemo/Source/LuaDemo/AdventureGameMode.cpp
@@ -1,5 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AdventureGameMode.h"
 
+void AAdventureGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World!"));
+}
--- LuaDemo/Source/LuaDemo/AdventureGameMode.h
+++ LuaDemo/Source/LuaDemo/AdventureGameMode.h
@@ -13,5 +13,7 @@ UCLASS()
 class LUADEMO_API AAdventureGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
+	virtual void StartPlay() override;
 	
 };
