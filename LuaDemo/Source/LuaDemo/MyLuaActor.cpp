c15ea2379b9b3ce296cf7e1be48cecce15d364b7
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-12-01 20:46:12 +0800

    feat: add lua actor

---
+++ LuaDemo/Source/LuaDemo/MyLuaActor.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyLuaActor.h"
+
+AMyLuaActor::AMyLuaActor() {
+	LuaFilePath = "MyLuaActor";
+}
---
+++ LuaDemo/Source/LuaDemo/MyLuaActor.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LuaActor.h"
+#include "MyLuaActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUADEMO_API AMyLuaActor : public ALuaActor
+{
+	GENERATED_BODY()
+public:
+	AMyLuaActor();
+};
