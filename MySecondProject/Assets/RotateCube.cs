2ee2701ae6bbd3de2559d459b0d1d908d0d3d61d
MJZHENG-PC3\mjzheng <mjzheng@tencent.com>
2025-12-10 10:19:34 +0800

    feat: update

---
+++ MySecondProject/Assets/RotateCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("立方体开始旋转了！");
+    }
+
+    void Update()
+    {
+        // 每帧旋转立方体
+        // 参数：每秒旋转的角度
+        transform.Rotate(0, 50 * Time.deltaTime, 0);
+    }
+}
